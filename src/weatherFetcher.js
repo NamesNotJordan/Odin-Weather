f58fd6aff53dfd498e3c4a3187783ccdc680ebac
Jayden <jayden.heberhendrickse@gmail.com>
2025-02-26 15:30:08 +0200

    Added the base

---
+++ src/weatherFetcher.js
@@ -0,0 +1,6 @@
+// Contains functions fo interacting with the weather API
+const WEATHER_API = "EVB43527DCXUA9T7NFZ4XCN7F";
+
+async function getWeather(location) {
+    const response = await fetch('call',{mode: 'cors'})
+}
