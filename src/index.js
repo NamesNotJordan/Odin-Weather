8ca59b036bfabbafcaa1459984071fbef2bfec41
Jayden <jayden.heberhendrickse@gmail.com>
2025-02-26 18:42:37 +0200

    renamed weather fetcher function. Assigned api request to submit button.  Basic styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import {getWeather} from "./weatherFetcher.js"
+import "./style.css";
+import {getWeatherToday} from "./weatherFetcher.js"
 
-console.log("making request");
-getWeather("London,UK").then((weatherData) => console.log(weatherData))
+document.getElementById("weather-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("making request");
+    getWeatherToday("London,UK").then((weatherData) => console.log(weatherData));
+})
--- src/weatherFetcher.js
+++ src/weatherFetcher.js
@@ -3,7 +3,7 @@ import {format} from "date-fns"
 
 const WEATHER_API = "EVB43527DCXUA9T7NFZ4XCN7F";
 
-export async function getWeather(location) {
+export async function getWeatherToday(location) {
     try {
         const currentDate = format(new Date(), "yyy-MM-dd");
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${currentDate}?key=${WEATHER_API}`,{mode: 'cors'}); 
