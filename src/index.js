e30e53a343ca9a6817d1fafb5a3058c0e97ee26d
Jayden <jayden.heberhendrickse@gmail.com>
2025-02-27 00:02:26 +0200

    added showForecast to display basic information about the current weather in a given location

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import {getWeatherToday} from "./weatherFetcher.js"
 document.getElementById("weather-form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("making request");
-    getWeatherToday("London,UK").then((weatherData) => console.log(weatherData));
+    getWeatherToday("London,UK").then((weatherData) => {
+        console.log(weatherData);
+        showForecast(weatherData);
+    });
 })
+
+function showForecast(weatherData) {
+    document.getElementById("forecast").style.display = "flex";
+    const forecastHeading = document.getElementById("forecast-heading");
+    const temperatureText = document.getElementById("temperature-text");
+    const conditionText = document.getElementById("condition-text");
+
+    const address = weatherData.address;
+    const temperature = weatherData.days[0].temp;
+    const conditions = weatherData.days[0].conditions
+
+    forecastHeading.innerHTML = `Forecast for ${address}`;
+    temperatureText.innerHTML = `${temperature}°C`;
+    conditionText.innerHTML = conditions;
+}
