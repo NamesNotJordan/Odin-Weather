6ab8036734f91f9bf9e6de597ac4bf84b784467f
Jayden <jayden.heberhendrickse@gmail.com>
2025-05-13 23:11:17 +0200

    added weather icons and updated getWeatherToday function to support temperature unit selection

--- src/index.js
+++ src/index.js
@@ -4,23 +4,50 @@ import {getWeatherToday} from "./weatherFetcher.js"
 document.getElementById("weather-form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("making request");
-    getWeatherToday("London,UK").then((weatherData) => {
+    const city = document.getElementById("city-input").value;
+    if(city === "") {
+        alert("Please enter a city name. Examples: \"London\" \"New York, NY\"'");
+        return;
+    }
+    const unitSystem = document.querySelector('input[name="temp-unit"]:checked').value;
+    getWeatherToday(city, unitSystem).then((weatherData) => {
         console.log(weatherData);
-        showForecast(weatherData);
+        renderCurrentForecast(weatherData);
+    }).catch( (error) => {
+        console.error("Error fetching weather data:", error);
+        alert("Unable to fetch weather data. Please try again later.");
     });
 })
 
-function showForecast(weatherData) {
-    document.getElementById("forecast").style.display = "flex";
+function renderCurrentForecast(weatherData) {
     const forecastHeading = document.getElementById("forecast-heading");
     const temperatureText = document.getElementById("temperature-text");
     const conditionText = document.getElementById("condition-text");
 
-    const address = weatherData.address;
-    const temperature = weatherData.days[0].temp;
-    const conditions = weatherData.days[0].conditions
+    const address = weatherData.resolvedAddress;
+    let temperature = weatherData.currentConditions.temp;
+    const conditions = weatherData.currentConditions.conditions;
+    let icon = weatherData.currentConditions.icon;
 
-    forecastHeading.innerHTML = `Forecast for ${address}`;
-    temperatureText.innerHTML = `${temperature}°C`;
+    forecastHeading.innerHTML = `<span>Forecast for</span> \n${address}`;
+    temperatureText.innerHTML = `${temperature}°${getTempuratureUnit()}`;
     conditionText.innerHTML = conditions;
+    setIcon(icon);
+    document.getElementById("forecast").classList.add("show");
+}
+
+function getTempuratureUnit () {
+    let system = document.querySelector('input[name="temp-unit"]:checked').value;
+    if (system === "us") {
+        return "F";
+    } else {
+        return "C";
+    }
+}
+function setIcon(iconID) {
+    const icon = document.getElementById("weather-icon");
+    const baseUrl = "https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/4th%20Set%20-%20Color";
+    icon.src = `${baseUrl}/${iconID}.svg`;
+    icon.alt = iconID;
+    icon.style.display = "block";
 }
--- src/weatherFetcher.js
+++ src/weatherFetcher.js
@@ -3,10 +3,10 @@ import {format} from "date-fns"
 
 const WEATHER_API = "EVB43527DCXUA9T7NFZ4XCN7F";
 
-export async function getWeatherToday(location) {
+export async function getWeatherToday(location, unit = "metric") {
     try {
         const currentDate = format(new Date(), "yyy-MM-dd");
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${currentDate}?key=${WEATHER_API}`,{mode: 'cors'}); 
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${currentDate}?key=${WEATHER_API}&unitGroup=${unit}&iconSet=icons2`,{mode: 'cors'}); 
         const weatherData = await response.json();
         return weatherData;
     } catch (error) {
